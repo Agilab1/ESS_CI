710301f523568d3c27d938d14a7ba050b75375cf
Punam Hon <punamhon5@gmail.com>
2025-11-14 18:03:02 +0530

    roles file is added

--- application/views/Role/role_form.php
+++ application/views/Role/role_form.php
@@ -1,46 +1,47 @@
-<div class="py-5">
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-7">
-                <div class="card shadow">
-                    <div class="card-header">
-                        <h5><?= $action ?>Role</h5>
-                    </div>
-                    <div class="card-body">
-                        <form action="<?= base_url('Role/save'); ?>" method="post" autocomplete="off" >
-                            <table class="table table-bordered">
-                                <input type="hidden" name="action" value="<?= $action ?>">
-                                <input type="hidden" name="old_staff_id" value="<?= $role->role_id ?? '' ?>">
+<table class="table table-bordered table-striped">
+      <a href="<?= base_url('Role/add'); ?>" class="btn btn-primary float-right">Add Staff</a>
+       <!-- Flash Messages -->
+            <?php include('assets/incld/messages.php') ?>
 
-                                <tr>
-                                    <td colspan="2">
-                                        <label class="form-label">Role ID</label>
-                                        <input type="text" name="role_id" id="" class="form-control" value="<?= $role->role_id ?? '' ?>" <?= ($action =='edit' ||$action =='view')? 'readonly': '' ?>>
-                                        <small class="text-danger"><?= form_error('role_id'); ?></small>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        <label class="form-control">User Role</label>
-                                        <input type="text" name="usr_role" class="form-control" value="<?= $role->usr_role ?? '' ?>" id="">
-                                        <small class="text-danger"><?= form_error('usr_role'); ?></small>
-                                    </td>
-                                    <td>
-                                        <label class="forn-control">Role Status</label>
-                                        <input type="checkbox" name="role_st" id="" value="<?= $role->role_st ?>" >
-                                        <small class="text-danger"><?= base_url('role_st') ?></small>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td colspan="2" class="text-center">
-                                        <button type="submit" class="btn btn-primary">Submit</button>
-                                    </td>
-                                </tr>
-                            </table>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+    <thead>
+        <tr>
+            <th>SR NO</th>
+            <th>Role ID</th>
+            <th>User Role</th>
+            <th>Status</th>
+            <th colspan="3" class="text-center">Action</th>
+        </tr>
+    </thead>
+
+    <tbody>
+        <?php if (!empty($roles)): ?>
+            <?php foreach ($roles as $i => $role): ?>
+                <tr>
+                    <td><?= $i + 1 ?></td>
+                    <td><?= $role->role_id ?></td>
+                    <td><?= $role->usr_role ?></td>
+                    <td><?= $role->role_st ?></td>
+
+                    <td class="text-center">
+                        <a href="<?= base_url('Role/view/'.$role->role_id) ?>">
+                            <i class="fa fa-eye"></i>
+                        </a>
+                    </td>
+
+                    <td class="text-center">
+                        <a href="<?= base_url('Role/edit/'.$role->role_id) ?>">
+                            <i class="fa fa-edit"></i>
+                        </a>
+                    </td>
+
+                    <td class="text-center">
+                        <a href="<?= base_url('Role/delete/'.$role->role_id) ?>" 
+                           onclick="return confirm('Are you sure?')">
+                            <i class="fa fa-trash text-danger"></i>
+                        </a>
+                    </td>
+                </tr>
+            <?php endforeach; ?>
+        <?php endif; ?>
+    </tbody>
+</table>
