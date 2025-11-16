5dbc70977213b62c695b74c521bef97a20fbacd2
Bhupesh Kumbhar <bhupesh.saptech@gmail.com>
2025-11-16 19:02:42 +0530

    Admin Controller Changed

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -6,7 +6,7 @@ class Admin extends CI_Controller {
 	public function __construct()
 {
     parent::__construct();
-    $this->load->model('Admin_model');
+    $this->load->model('User_model');
 
     
     if (!$this->session->userdata('logged_in')) {
