a3d0eeb9c8b9aefb40b555742b47b09daa5548d3
Aditya Kamble <127715039+AdityaKamble11@users.noreply.github.com>
2025-12-02 22:44:35 +0530

    Updated

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 |
 */
 // $config['base_url'] = 'http://localhost/ESS_TEST/';
-$config['base_url'] = 'https://agilesaptech.com/ESS_TEST/';
+$config['base_url'] = 'https://agilesaptech.com/ESS_CI/';
  //$config['base_url'] = 'http://localhost/ESS_CI/';
 
 /*
