55dd6be87f3483d49797dacee0401d452767f449
Aditya Kamble <127715039+AdityaKamble11@users.noreply.github.com>
2025-12-02 17:33:43 +0530

    Datatable added

--- application/views/Holiday/list.php
+++ application/views/Holiday/list.php
@@ -1,30 +1,30 @@
 <style>
-    /* FIX HEADER TEXT ROTATION / VERTICAL STACKING */
-    th {
-        white-space: nowrap !important;
-        writing-mode: horizontal-tb !important;
-        transform: rotate(0deg) !important;
-        text-orientation: mixed !important;
-    }
-
+    th,
     td {
         white-space: nowrap;
+    }
+
+    th {
+        writing-mode: horizontal-tb !important;
+        transform: rotate(0deg) !important;
     }
 
     table {
         table-layout: auto !important;
     }
+
+    /* Move Add button right */
     .card-header a.btn {
-    margin-left: auto !important; /* push button to right */
-}
+        margin-left: auto !important;
+    }
 </style>
 
 
 <div class="card">
-    <div class="card-header d-flex flex-wrap justify-content-between align-items-center">
-        <h4 class="mb-2 mb-md-0">Holiday Details</h4>
 
-        <a href="<?= base_url('Holiday/add'); ?>" class="btn btn-primary mt-2 mt-md-0">
+    <div class="card-header d-flex justify-content-between align-items-center">
+        <h4 class="mb-0">Holiday Details</h4>
+        <a href="<?= base_url('Holiday/add'); ?>" class="btn btn-primary">
             <i class="fa fa-plus"></i> Add Holiday
         </a>
     </div>
@@ -38,17 +38,16 @@
             <div class="alert alert-danger"><?= $this->session->flashdata('error'); ?></div>
         <?php endif; ?>
 
-        <!-- RESPONSIVE WRAPPER -->
         <div class="table-responsive">
-            <table id="holidayTable" class="table table-bordered table-striped align-middle" style="font-size: 14px;">
+            <table id="dtbl" class="table table-bordered table-striped">
 
                 <thead class="btn-primary text-white">
                     <tr>
-                        <th>#</th>
+                        <th style="width:4vw;">#</th>
                         <th>Holiday Date</th>
                         <th>Day Category</th>
                         <th>Description</th>
-                        <th class="text-center" colspan="3">Action</th>
+                        <th style="width:6vw;" class="text-center">Action</th>
                     </tr>
                 </thead>
 
@@ -61,37 +60,31 @@
                                 <td><?= $h->day_cat ?></td>
                                 <td><?= $h->day_txt ?></td>
 
-                                <!-- VIEW -->
-                                <td class="text-center">
-                                    <a href="<?= base_url('Holiday/view/' . $h->date_id); ?>">
+                                <!-- ACTION BUTTONS (Single TD like User Table) -->
+                                <td class="text-center" style="white-space: nowrap;">
+
+                                    <a href="<?= base_url('Holiday/view/' . $h->date_id); ?>" class="mx-1">
                                         <i class="fa fa-eye"></i>
                                     </a>
-                                </td>
 
-                                <!-- EDIT -->
-                                <td class="text-center">
-                                    <a href="<?= base_url('Holiday/edit/' . $h->date_id); ?>">
+                                    <a href="<?= base_url('Holiday/edit/' . $h->date_id); ?>" class="mx-1">
                                         <i class="fa fa-edit"></i>
                                     </a>
-                                </td>
 
-                                <!-- DELETE -->
-                                <td class="text-center">
                                     <a href="<?= base_url('Holiday/delete/' . $h->date_id); ?>"
-                                        onclick="return confirm('Delete holiday on date: <?= $h->date_id ?> ?');">
+                                        onclick="return confirm('Delete holiday on <?= $h->date_id ?> ?');"
+                                        class="mx-1">
                                         <i class="fa fa-trash text-danger"></i>
                                     </a>
+
                                 </td>
                             </tr>
                         <?php endforeach; ?>
 
                     <?php else: ?>
                         <tr>
-                            <td colspan="7" class="text-center text-muted">
-                                No holidays found.
-                            </td>
+                            <td colspan="5" class="text-center text-danger">No Holidays Found</td>
                         </tr>
-
                     <?php endif; ?>
                 </tbody>
 
