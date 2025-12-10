559709ed1b3eeda320db18397430b3ce592737cc
Punam Hon <punamhon5@gmail.com>
2025-12-10 12:17:22 +0530

    location updated

--- application/views/user/dashboard.php
+++ application/views/user/dashboard.php
@@ -102,7 +102,7 @@
             <div class="icon">
               <i class="ion ion-pie-graph"></i>
             </div>
-            <a href="location/list.php" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            <a href="location/add.php" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
           </div>
         </div>
       </div>
