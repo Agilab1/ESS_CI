6f519222712fd465e9eeca4ba80c64d16aa7e93c
Punam Hon <punamhon5@gmail.com>
2025-12-10 13:57:43 +0530

    Count added in location

--- application/views/user/dashboard.php
+++ application/views/user/dashboard.php
@@ -95,7 +95,7 @@
         <div class="col-lg-2 col-6">
           <div class="small-box bg-dark">
             <div class="inner">
-              <h3>0</h3>
+            <h3><?= $counts->cnt6 ?></h3>
 
               <p>Locations</p>
             </div>
