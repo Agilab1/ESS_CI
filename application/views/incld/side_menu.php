c1951d548b47df694868b0491bc6ebbeb06be6a2
Aditya Kamble <127715039+AdityaKamble11@users.noreply.github.com>
2025-12-30 17:03:35 +0530

    UI updated

--- application/views/incld/side_menu.php
+++ application/views/incld/side_menu.php
@@ -3,7 +3,7 @@
     <div  style ="color:white; text-align:center";>Employee Self Service Portal</div>
     <div>
     <a href="../index.php" class="brand-link">
-      <img  src="assets/dist/img/ess.jpg" alt="WSS Logo" style="width:230px;height:60px;opacity: .8; ">
+      <img  src="assets/dist/img/ess.jpg" alt="WSS Logo" style="width:230px;height:60px;opacity: .8; border-radius: 11px; ">
     </a>
     </div>
 
