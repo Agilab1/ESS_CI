2b70d18602a04a3f49eeb4406fdc5cd815b40a85
Dhiraj Shinde <ashishshinde16177@gmail.com>
2025-12-31 17:47:43 +0530

    updated

--- application/views/Bom/material_bom.php
+++ application/views/Bom/material_bom.php
@@ -35,8 +35,7 @@
                         <thead class="table-primary">
                             <form action="">
                                 <tr>
-                                    <!-- <th><?= $i++ ?></th>  -->
-                                     <!-- <input type=""> -->
+                                   
                                     <th><input type="text" value="#" readonly></th>
                                     <th><input type="text" value="BOM ID" readonly></th>
                                     <th> <input type="text" value="Child Material" readonly> </th>
@@ -45,15 +44,7 @@
                                     <th><input type="text" value="Action" readonly> </th>
                                 </tr>
                             </form>
-                            <!-- <tr>
-
-                                <th>#</th>
-                                <th>BOM ID</th>
-                                <th>Child Material</th>
-                                <th>UOM</th>
-                                <th>Qty</th>
-                                <th>Action</th>
-                            </tr> -->
+                          
                         </thead>
 
                         <tbody>
@@ -61,33 +52,20 @@
                                 <?php $i = 1;
                                 foreach ($boms as $b): ?>
                                     <form action="">
-                                        <!-- <tr>
-                                        <td><?= $i++ ?></td> -->
-                                        <!-- <td><input type="<?= $b->bom_id ?>"></td> -->
-                                        <!-- <td><?= $b->bom_id ?></td>
-                                        <td><?= $b->child_name ?></td>
-                                        <td><?= $b->uom ?></td>
-                                        <td><?= $b->qty ?></td>
-                                        <td>
-                                            <a href="<?= base_url('bom/view/' . $b->bom_id) ?>"><i class="fa fa-eye"></i></a>
-                                            <a href="<?= base_url('bom/edit/' . $b->bom_id) ?>"><i class="fa fa-edit text-primary"></i></a>
-                                            <a href="<?= base_url('bom/delete/' . $b->bom_id) ?>"
-                                                onclick="return confirm('Delete BOM?')">
-                                                <i class="fa fa-trash text-danger"></i>
-                                            </a>
-                                        </td>
-                                    </tr> -->
-
+                                        
+                                        <form action="">
+                                            <tr>
+                                                <td><input type="text" value=""></td>
+                                            </tr>
+                                        </form>
                                         <tr>
+                                           
                                             <td> <input type="text" value="<?= $i++ ?>" readonly ></td>
                                             <td><input type="text" value="<?= $b->bom_id ?>" readonly></td>
                                             <td><input type="text" value="<?= $b->child_name ?>" readonly></td>
                                             <td><input type="text" value="<?= $b->uom ?>" readonly> </td>
                                             <td><input type="text" value="<?= $b->qty ?>" readonly></td>
-                                            <!-- <td><?= $b->bom_id ?></td>
-                                        <td><?= $b->child_name ?></td>
-                                        <td><?= $b->uom ?></td>
-                                        <td><?= $b->qty ?></td> -->
+                                           
                                             <td>
                                                 <a href="<?= base_url('bom/view/' . $b->bom_id) ?>"><i class="fa fa-eye"></i></a>
                                                 <a href="<?= base_url('bom/edit/' . $b->bom_id) ?>"><i class="fa fa-edit text-primary"></i></a>
