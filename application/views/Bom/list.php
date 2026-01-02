574552ea11ddbfbd1bf81c88a4f8e4da5225331c
Aditya Kamble <127715039+AdityaKamble11@users.noreply.github.com>
2026-01-02 12:27:28 +0530

    button alignment

--- application/views/Bom/list.php
+++ application/views/Bom/list.php
@@ -1,9 +1,10 @@
 <div class="card">
     <div class="card-header d-flex justify-content-between align-items-center">
         <h4 class="mb-0">BOM – Bill of Material</h4>
-        <a href="<?= base_url('bom/add'); ?>" class="btn btn-primary">
+        <a href="<?= base_url('bom/add'); ?>" class="btn btn-primary float-end">
             <i class="fa fa-plus"></i> Add BOM
         </a>
+
     </div>
 
     <div class="card-body">
